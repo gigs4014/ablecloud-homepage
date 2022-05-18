0fab472d12caf14ba33f9e5fdf6c9906367bdda4
Jaehun Kim <wogns3623@gmail.com>
2022-05-18 18:42:18 +0900

    Add FeedGenerator
    - Post 객체를 받아 rss/atom feed xml을 생성해주는 FeedGenerator를 추가함

---
+++ src/helpers/post/feed.ts
@@ -0,0 +1,36 @@
+import { Feed, FeedOptions } from 'feed';
+
+import { Post } from '@/types';
+
+export class FeedGenerator {
+  public defaultOptions: Partial<FeedOptions>;
+  constructor(defaultOptions?: Partial<FeedOptions>) {
+    this.defaultOptions = defaultOptions ?? {};
+  }
+
+  public fromPosts(posts: Post[], options?: FeedOptions): Feed {
+    const feed = new Feed({ copyright: '', id: '', title: '', ...this.defaultOptions, ...options });
+
+    posts.forEach(post => {
+      feed.addItem({
+        title: post.title,
+        id: post.slug,
+        link: `${feed.options.link}/blog/${[...post.categories, post.slug].join('/')}`,
+        date: new Date(post.date),
+      });
+    });
+
+    return feed;
+  }
+}
+
+export default new FeedGenerator({
+  title: '에이블클라우드 블로그',
+  description: '제품 및 기능 릴리즈, 관련 기술, 사례 소개',
+  copyright: '© ABLECLOUD with ABLESTOR, since 2021',
+  link: process.env.HOST_URL,
+  feedLinks: {
+    atom: `${process.env.HOST_URL}/feed/atom`,
+    rss: `${process.env.HOST_URL}/feed/rss`,
+  },
+});
--- src/helpers/post/index.ts
+++ src/helpers/post/index.ts
@@ -1,1 +1,2 @@
+export { default as FeedGenerator } from './feed';
 export * from './post';
