b50103f7696c3f3adeadbf15ade462b4d933c2d7
Jaehun Kim <wogns3623@gmail.com>
2022-05-19 15:15:21 +0900

    Add tailwind spacing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,19 @@
+//@ts-check
+/** @typedef { import('tailwindcss/tailwind-config').TailwindConfig } TailwindConfig */
+/** @typedef { import('tailwindcss/tailwind-config').TailwindThemeValue } TailwindThemeValue */
+
+/**
+ * @type {TailwindThemeValue}
+ */
+const spacing = {
+  128: '32rem',
+  144: '36rem',
+  'page-full': '1200px',
+};
+
+/**
+ * @type {TailwindConfig}
+ */
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
@@ -7,10 +23,9 @@ module.exports = {
         1: '1px',
         0.5: '0.5px',
       },
-      spacing: {
-        128: '32rem',
-        144: '36rem',
-      },
+      spacing,
+      maxWidth: spacing,
+      minWidth: spacing,
     },
   },
   plugins: [require('@tailwindcss/typography')],
