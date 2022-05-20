9f51f543d2e26c68325ef43b479f267e761b6f4d
Jaehun Kim <wogns3623@gmail.com>
2022-05-20 18:07:07 +0900

    Modify Carousel to accept ref props

--- src/components/common/Carousel/Carousel.tsx
+++ src/components/common/Carousel/Carousel.tsx
@@ -1,53 +1,122 @@
-import { useEffect, useRef } from 'react';
+import { RefObject, forwardRef, useCallback, useEffect, useImperativeHandle, useRef } from 'react';
 
+import { useMountEffect } from '@/hooks/utils';
 import { BaseComponentProps } from '@/types';
 import { cls } from '@/utils';
 
 export interface CarouselProps extends BaseComponentProps {
   children?: React.ReactNode;
+
+  /**
+   * @default false
+   */
+  autoScrollOnMount?: boolean;
+
+  /**
+   * @default true
+   */
+  stopAutoScrollOnEnd?: boolean;
+
+  /**
+   * @default 5000 ms
+   */
+  autoScrollInterval?: number;
 }
 
-function Carousel({ className, children }: CarouselProps) {
-  const carouselRef = useRef<HTMLUListElement>(null);
-  const prevCarouselPosition = useRef<number>(0);
+export interface CarouselRef {
+  startAutoScroll: () => void;
+  stopAutoScroll: () => void;
+  elementRef: RefObject<HTMLUListElement>;
+}
 
-  useEffect(() => {
-    if (!carouselRef.current) return;
+const Carousel = forwardRef<CarouselRef, CarouselProps>(
+  (
+    {
+      className,
+      children,
+      autoScrollOnMount = false,
+      stopAutoScrollOnEnd = true,
+      autoScrollInterval = 5000,
+    },
+    ref,
+  ) => {
+    const elementRef = useRef<HTMLUListElement>(null);
+    const prevCarouselPosition = useRef<number>(0);
+    const autoScrollIntervalId = useRef<NodeJS.Timeout>();
 
-    const interval = setInterval(() => {
-      if (!carouselRef.current) {
-        clearInterval(interval);
-        return;
-      }
+    const stopAutoScroll = useCallback(
+      () => autoScrollIntervalId.current && clearInterval(autoScrollIntervalId.current),
+      [],
+    );
 
-      if (prevCarouselPosition.current === carouselRef.current.scrollLeft) {
-        clearInterval(interval);
-        return;
-      }
+    const startAutoScroll = useCallback(() => {
+      if (elementRef.current === null || autoScrollIntervalId.current !== undefined) return;
 
-      prevCarouselPosition.current = carouselRef.current.scrollLeft;
-
-      carouselRef.current.scrollBy({
+      elementRef.current.scrollBy({
         behavior: 'smooth',
         left: 1,
       });
-    }, 7.5 * 1000);
 
-    carouselRef.current.scrollBy({
-      behavior: 'smooth',
-      left: 1,
+      autoScrollIntervalId.current = setInterval(() => {
+        if (!elementRef.current) {
+          stopAutoScroll();
+          return;
+        }
+
+        if (stopAutoScrollOnEnd) {
+          if (prevCarouselPosition.current === elementRef.current.scrollLeft) {
+            stopAutoScroll();
+            return;
+          }
+        }
+
+        prevCarouselPosition.current = elementRef.current.scrollLeft;
+
+        elementRef.current.scrollBy({
+          behavior: 'smooth',
+          left: 1,
+        });
+      }, autoScrollInterval);
+    }, [stopAutoScroll, autoScrollInterval, stopAutoScrollOnEnd]);
+
+    // refresh autoScrollInterval when startAutoScroll changes
+    useEffect(() => {
+      if (autoScrollIntervalId.current) {
+        stopAutoScroll();
+        startAutoScroll();
+      }
+    }, [stopAutoScroll, startAutoScroll]);
+
+    useMountEffect(() => {
+      // start autoScroll when component is mounted && autoScroll is true
+      if (autoScrollOnMount) {
+        startAutoScroll();
+      }
+
+      // stop autoScroll when component is unmounted
+      return stopAutoScroll;
     });
 
-    return () => clearInterval(interval);
-  }, []);
+    useImperativeHandle(
+      ref,
+      () => ({
+        startAutoScroll,
+        stopAutoScroll,
+        elementRef,
+      }),
+      [startAutoScroll, stopAutoScroll],
+    );
 
-  return (
-    <ul
-      className={cls`flex snap-x snap-mandatory space-x-8 overflow-x-scroll scroll-smooth px-8 scrollbar-none ${className}`}
-      ref={carouselRef}>
-      {children}
-    </ul>
-  );
-}
+    return (
+      <ul
+        className={cls`flex snap-x snap-mandatory space-x-8 overflow-x-scroll scroll-smooth px-8 scrollbar-none ${className}`}
+        ref={elementRef}>
+        {children}
+      </ul>
+    );
+  },
+);
+
+Carousel.displayName = 'Carousel';
 
 export default Carousel;
