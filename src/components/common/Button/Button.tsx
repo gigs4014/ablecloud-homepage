1400cb9e8bfe31235873cfa05bc9ad1eebb3f85a
Jaehun Kim <wogns3623@gmail.com>
2022-05-20 13:40:32 +0900

    Add Button component

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,55 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+
+import { BooleanProps, cls, useBooleanPropParser } from '@/utils';
+
+const ButtonStyleUnionList = ['solid', 'bordered'] as const;
+type ButtonStyleUnion = typeof ButtonStyleUnionList[number];
+const buttonBooleanProps = {
+  buttonStyle: ButtonStyleUnionList,
+};
+
+export interface ButtonProps
+  extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {}
+
+function Button(buttonProps: ButtonProps & BooleanProps<{ buttonStyle?: ButtonStyleUnion }>) {
+  const { buttonStyle = 'solid', ...props } = useBooleanPropParser(buttonProps, buttonBooleanProps);
+
+  if (buttonStyle === 'solid') {
+    return <SolidButton {...props} />;
+  } else if (buttonStyle === 'bordered') {
+    return <BorderedButton {...props} />;
+  } else {
+    if (process.env.NODE_ENV === 'development') {
+      throw new Error(`Unknown button style: ${buttonStyle}`);
+    } else {
+      console.error(`Unknown button style: ${buttonStyle}`);
+      return null;
+    }
+  }
+}
+
+export default Button;
+
+export function BorderedButton(buttonProps: ButtonProps) {
+  const { children, className, ...rest } = buttonProps;
+
+  return (
+    <button
+      {...rest}
+      className={cls`self-center rounded-md border-0.5 border-sky-500 py-2 px-10 text-sky-500 ${className}`}>
+      {children}
+    </button>
+  );
+}
+
+export function SolidButton(buttonProps: ButtonProps) {
+  const { children, className, ...rest } = buttonProps;
+
+  return (
+    <button
+      {...rest}
+      className={cls`self-center rounded-md bg-sky-500 py-2 px-10 text-white ${className}`}>
+      {children}
+    </button>
+  );
+}
