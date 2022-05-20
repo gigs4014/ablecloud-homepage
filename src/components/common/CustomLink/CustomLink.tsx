09456b155d01c451884e2c9110ed323fb20629bb
Jaehun Kim <wogns3623@gmail.com>
2022-05-20 14:29:44 +0900

    Add hoverBehavoir prop to CustomLink

--- src/components/common/CustomLink/CustomLink.tsx
+++ src/components/common/CustomLink/CustomLink.tsx
@@ -1,33 +1,49 @@
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 
 import Link, { LinkProps } from 'next/link';
+
+import { cls } from '@/utils';
 
 export interface CustomLinkProps extends Omit<LinkProps, 'href'> {
   href?: LinkProps['href'];
   children: ReactNode;
   style?: React.CSSProperties;
   className?: string;
+  /**
+   * @default color (change to rgb(96, 165, 250))
+   */
+  hoverBehavoir?: 'color' | 'underline' | 'none';
 }
 
 export default function CustomLink({
   href,
   children,
-  className,
+  className: givenClassName,
   style,
+  hoverBehavoir,
   ...linkProps
 }: CustomLinkProps) {
+  const className = useMemo(
+    () =>
+      cls`${{
+        'hover:text-blue-400': hoverBehavoir === 'color',
+        'hover:underline': hoverBehavoir === 'underline',
+      }} ${givenClassName}`,
+    [givenClassName, hoverBehavoir],
+  );
+
   // Must add passHref to Link
   if (href) {
     return (
       <Link href={href} passHref {...linkProps}>
-        <a className={`hover:text-blue-400 ${className}`} style={style}>
+        <a className={className} style={style}>
           {children}
         </a>
       </Link>
     );
   } else {
     return (
-      <div className={`hover:text-blue-400 ${className}`} style={style}>
+      <div className={className} style={style}>
         {children}
       </div>
     );
