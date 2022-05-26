d105d980c200fb9fcf3b00613f1a8326617c864b
Jaehun Kim <wogns3623@gmail.com>
2022-05-26 15:58:25 +0900

    Remove default icon values

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,9 +1,16 @@
+import { CustomLink } from '@/components/common';
+
 export default function Footer() {
   return (
     <footer className='flex h-24 w-full items-center justify-center border-t'>
       {/* simplified logo */}
       {/* copyright */}
       {/* social logos */}
+      <section className='flex space-x-4'>
+        <CustomLink href='/feed'>
+          <span className='rounded-sm bg-orange-400 text-white icon-round-[rss\_feed]'></span>
+        </CustomLink>
+      </section>
       {/* pages not in header */}
     </footer>
   );
