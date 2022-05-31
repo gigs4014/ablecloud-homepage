18324d4257e327cfd73752fd8ae906a88c525a7a
Jaehun Kim <wogns3623@gmail.com>
2022-05-31 18:47:40 +0900

    Add stories home page

---
+++ src/pages/stories/index.tsx
@@ -0,0 +1,85 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+
+import { getPosts } from '@/helpers/post';
+import { BasePageProps, Post } from '@/types';
+
+import { CustomLink } from '@/components/common';
+import { Container } from '@/components/layout';
+
+interface StoryHomePageProps extends BasePageProps {
+  stories: Post[];
+}
+
+export default function StoryHomePage({ stories }: StoryHomePageProps) {
+  return (
+    <>
+      <Head>
+        <title>성공사례 - 에이블클라우드</title>
+      </Head>
+
+      <Container.Article className='prose-ul:p-0'>
+        <header className='mt-16'>
+          <Container.PageWidth>
+            <h2>ABLESTACK과 함께 IT 혁신을 실현한 사례들을 확인해보세요.</h2>
+          </Container.PageWidth>
+        </header>
+
+        <Container.PageWidth>
+          <ul className='grid grid-cols-1 gap-8 md:grid-cols-2'>
+            {stories.map(
+              (
+                { title, categories, slug, tags = [], frontmatter: { subtitle, thumbnail } = {} },
+                index,
+              ) => (
+                <li key={index}>
+                  <CustomLink href={[...categories, slug].join('/')} hoverBehavoir='none'>
+                    <Container.Card className='flex h-full flex-col'>
+                      {/* image placeholder */}
+                      <div className='h-64 w-full bg-slate-200' />
+
+                      <section className='flex flex-1 flex-col justify-between p-3 text-left'>
+                        <div>
+                          {/* title */}
+                          <h2>{title}</h2>
+
+                          {/* subtitle */}
+                          <p className='font-thin text-gray-500'>{String(subtitle)}</p>
+                        </div>
+
+                        <div>
+                          <hr className='my-2' />
+
+                          {/* tags */}
+                          <div className='flex text-sm text-gray-300'>
+                            {tags.map((tag, tagIndex) => (
+                              <span
+                                className='after:mx-1 after:content-["/"] last:after:hidden'
+                                key={tagIndex}>
+                                {tag}
+                              </span>
+                            ))}
+                          </div>
+                        </div>
+                      </section>
+                    </Container.Card>
+                  </CustomLink>
+                </li>
+              ),
+            )}
+          </ul>
+        </Container.PageWidth>
+      </Container.Article>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps<StoryHomePageProps> = async () => {
+  const stories = await getPosts(['stories']);
+
+  return {
+    props: {
+      stories,
+    },
+  };
+};
