10837fee6d5b74da18006c0ba6cc584d9af5741e
khun <woghd567@naver.com>
2022-08-05 14:44:34 +0900

    FIX: Footer 수정

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,54 +1,36 @@
+import { footerMenus } from '@/constants';
+import { v4 as uuid } from 'uuid';
+
+import { cls } from '@/utils';
+
+import { CustomLink } from '@/components/common';
+
 import Logo_ablecloud_gray from '@/public/images/logos/ablecloud_logo_gray.svg';
 
 function FooterSite() {
   return (
     <section className={'h-[330px] py-[50px]'}>
       <ul className={`flex list-none justify-between`}>
-        <li className={'min-w-50 float-left'}>
-          <ol className={'bg-red'}>
-            <li>에이블클라우드</li>
-            <li>회사소개</li>
-          </ol>
-        </li>
-        <li className={'min-w-50 min-h-30 float-left'}>
-          <ol>
-            <li>제품</li>
-            <li>ABLESTACK의 탄생</li>
-            <li>ABLESTACK 소개</li>
-          </ol>
-        </li>
-        <li className={'min-w-50 float-left'}>
-          <ol>
-            <li>솔루션</li>
-            <li>-</li>
-            <li>-</li>
-          </ol>
-        </li>
-        <li className={'min-w-50 float-left'}>
-          <ol>
-            <li>성공사례</li>
-            <li>-</li>
-            <li>-</li>
-          </ol>
-        </li>
-        <li className={'min-w-50 float-left'}>
-          <ol>
-            <li>파트너</li>
-            <li>파트너 네트워크</li>
-          </ol>
-        </li>
-        <li className={'min-w-50 float-left'}>
-          <ol>
-            <li>지원</li>
-            <li>데모 신청하기</li>
-            <li>전문가와 상담하기</li>
-            <li>기술 지원 받기</li>
-          </ol>
-        </li>
+        {footerMenus.map(menu => (
+          <li key={uuid()} className={'min-w-50 float-left'}>
+            <ol className={'bg-red'}>
+              <li className='font-[700]'>{menu.label}</li>
+              {menu.subMenuItems.map(subMenu => (
+                <CustomLink
+                  key={uuid()}
+                  href={subMenu.href}
+                  className={cls`mt-[8px] flex h-full w-full items-center justify-between`}>
+                  <li>{subMenu.label}</li>
+                </CustomLink>
+              ))}
+            </ol>
+          </li>
+        ))}
       </ul>
     </section>
   );
 }
+
 export default function Footer() {
   return (
     <footer className={`w-full max-w-page-full`}>
