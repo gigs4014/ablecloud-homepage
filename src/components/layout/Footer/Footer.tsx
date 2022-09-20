6ab5ab108f298c8d992976da32a4d4c982667702
ablesotre <ablesotre@ablesotreui-Macmini-3.local>
2022-09-20 16:35:00 +0900

    FIX: footer 버그 수정

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,16 +1,16 @@
-import { footerMenus } from '@/constants';
+import { footerInformation, footerMenus } from '@/constants';
 import { v4 as uuid } from 'uuid';
 
 import { cls } from '@/utils';
 
 import { CustomLink } from '@/components/common';
 
-import Logo_ablecloud_gray from '@/public/images/logos/ablecloud_logo_gray.svg';
+import Logo_ablecloud_white from '@/public/images/logos/ablecloud_logo_white.svg';
 
 function FooterSite() {
   return (
-    <section className={'hidden h-[330px] py-[50px] lg:block'}>
-      <ul className={`flex max-w-page-full list-none justify-between`}>
+    <section className={'relative hidden h-[227px] pt-[49px] pb-[7px] lg:block'}>
+      <ul className={` m-auto flex w-[1200px] max-w-page-full list-none justify-between`}>
         {footerMenus.map(menu => (
           <li key={uuid()} className={'min-w-50 float-left'}>
             <ol className={'bg-red'}>
@@ -31,16 +31,58 @@ function FooterSite() {
   );
 }
 
+function FooterInformation() {
+  return (
+    <section className={'relative flex h-[150px] bg-[#414141]'}>
+      <div className={'m-auto flex w-[1200px] shrink-0 grow-0 items-center justify-center'}>
+        <div>
+          <Logo_ablecloud_white className='mx-[22.5px]' />
+        </div>
+        <div>
+          {footerInformation.map((info, index) => {
+            if (index === 0) {
+              {
+                info.map(({ label, description }) => {
+                  return (
+                    <p
+                      key={uuid()}
+                      className={`mr-[16px] mb-[16px] inline-block leading-[23px] tracking-wide text-white`}>
+                      <label className={'font-bold'}>{label}</label>
+                      <label className={'font-normal'}>{description}</label>
+                    </p>
+                  );
+                });
+              }
+            }
+            return (
+              <>
+                {info.map(({ label, description }) => {
+                  return (
+                    <p
+                      key={uuid()}
+                      className={`mr-[18.75px] mb-[0px] inline-block leading-[23px] tracking-wide text-white`}>
+                      <label className={'font-bold'}>{label}</label>
+                      <label className={'font-normal'}>{description}</label>
+                    </p>
+                  );
+                })}
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
+
 export default function Footer() {
   return (
-    <footer className={`w-full max-w-page-full`}>
+    <footer className={`relative w-full`}>
       {/* simplified logo */}
       {/* copyright */}
       {/* social logos */}
       <FooterSite />
-      <div className={'mx-2.5 my-2.5 flex justify-start'}>
-        <Logo_ablecloud_gray />
-      </div>
+      <FooterInformation />
       {/* pages not in header */}
     </footer>
   );
--- src/constants/menus.ts
+++ src/constants/menus.ts
@@ -39,7 +39,24 @@ import { HeaderMenuItem } from '@/types';
 export const menuItems: Array<HeaderMenuItem> = [
   {
     label: '회사소개',
-    href: '/company',
+    subMenuItems: [
+      {
+        label: 'About US',
+        href: '/company',
+      },
+      {
+        label: '파트너',
+        href: '/company',
+      },
+      {
+        label: '문의하기',
+        href: '/company',
+      },
+      {
+        label: '찾아오시는 길',
+        href: '/company',
+      },
+    ],
   },
   {
     label: '제품',
@@ -50,130 +67,151 @@ export const menuItems: Array<HeaderMenuItem> = [
         href: '/products/ablestack',
       },
       {
-        type: 'group',
-        label: '인프라 스트럭처',
-        href: '/products/ablestack',
-        subMenuItems: [
-          { label: 'Glue 스토리지', href: '/products/ablestack/glue' },
-          { label: 'Cell 가상화', href: '/products/ablestack/cell' },
-          { label: 'Koral 쿠버네테스 엔진', href: '/products/ablestack/koral' },
-          { label: 'Track 네트워크 보안', href: '/products/ablestack/track' },
-        ],
-        // subMenuItems: [
-        //   { label: 'Block 블록스토리지', href: '/products/ablestack/block' },
-        //   { label: 'Cell 가상화', href: '/products/ablestack/cell' },
-        //   { label: 'Files 파일시스템', href: '/products/ablestack/files' },
-        //   { label: 'Genie 하이브리드 배포 자동화', href: '/products/ablestack/genie' },
-        //   { label: 'Glue 스토리지', href: '/products/ablestack/glue' },
-        //   { label: 'Koral 쿠버네테스 엔진', href: '/products/ablestack/koral' },
-        //   { label: 'Mold 클라우드 관리', href: '/products/ablestack/mold' },
-        //   { label: 'Object 오브젝트 스토리지', href: '/products/ablestack/object' },
-        //   { label: 'Slio', href: '/products/ablestack/slio' },
-        //   { label: 'Station 플랫폼서비스', href: '/products/ablestack/station' },
-        //   { label: 'Track 네트워크 보안', href: '/products/ablestack/track' },
-        //   { label: 'Wall 통합 모니터링', href: '/products/ablestack/wall' },
-        //   { label: 'Works 가상데스크톱', href: '/products/ablestack/works' },
-        //   { label: 'Core', href: '/products/ablestack/core' },
-        //   { label: 'Management', href: '/products/ablestack/management' },
-        // ],
+        label: 'Core',
+        href: 'products/ablestack',
       },
       {
-        type: 'group',
-        label: '관리플랫폼',
-        href: '/products/ablestack',
-        subMenuItems: [
-          { label: 'Mold 클라우드 관리', href: '/products/ablestack/mold' },
-          { label: 'Genie 하이브리드 배포 자동화', href: '/products/ablestack/genie' },
-          { label: 'Wall 통합 모니터링', href: '/products/ablestack/wall' },
-        ],
+        label: 'management',
+        href: 'products/ablestack',
       },
       {
-        type: 'group',
-        label: '통합스토리지',
-        href: '/products/ablestack',
-        subMenuItems: [
-          { label: 'Block 블록스토리지', href: '/products/ablestack/block' },
-          { label: 'Files 파일시스템', href: '/products/ablestack/files' },
-          { label: 'Object 오브젝트 스토리지', href: '/products/ablestack/object' },
-          { label: 'Slio 백업 자동화', href: '/products/ablestack/slio' },
-        ],
-      },
-      {
-        type: 'group',
-        label: '서비스 및 플랫폼',
-        href: '/products/ablestack',
-        subMenuItems: [
-          { label: 'Works 가상데스크톱', href: '/products/ablestack/works' },
-          { label: 'Station 플랫폼서비스', href: '/products/ablestack/station' },
-        ],
-      },
-      {
-        label: 'ABLESTACK 어플라이언스',
+        label: 'AbleStack 어플라이언스',
         href: '/products/ablestack',
       },
+      // {
+      // type: 'group',
+      // label: '인프라 스트럭처',
+      // href: '/products/ablestack',
+      // subMenuItems: [
+      //   { label: 'Glue 스토리지', href: '/products/ablestack/glue' },
+      //   { label: 'Cell 가상화', href: '/products/ablestack/cell' },
+      //   { label: 'Koral 쿠버네테스 엔진', href: '/products/ablestack/koral' },
+      //   { label: 'Track 네트워크 보안', href: '/products/ablestack/track' },
+      // ],
+      // subMenuItems: [
+      //   { label: 'Block 블록스토리지', href: '/products/ablestack/block' },
+      //   { label: 'Cell 가상화', href: '/products/ablestack/cell' },
+      //   { label: 'Files 파일시스템', href: '/products/ablestack/files' },
+      //   { label: 'Genie 하이브리드 배포 자동화', href: '/products/ablestack/genie' },
+      //   { label: 'Glue 스토리지', href: '/products/ablestack/glue' },
+      //   { label: 'Koral 쿠버네테스 엔진', href: '/products/ablestack/koral' },
+      //   { label: 'Mold 클라우드 관리', href: '/products/ablestack/mold' },
+      //   { label: 'Object 오브젝트 스토리지', href: '/products/ablestack/object' },
+      //   { label: 'Slio', href: '/products/ablestack/slio' },
+      //   { label: 'Station 플랫폼서비스', href: '/products/ablestack/station' },
+      //   { label: 'Track 네트워크 보안', href: '/products/ablestack/track' },
+      //   { label: 'Wall 통합 모니터링', href: '/products/ablestack/wall' },
+      //   { label: 'Works 가상데스크톱', href: '/products/ablestack/works' },
+      //   { label: 'Core', href: '/products/ablestack/core' },
+      //   { label: 'Management', href: '/products/ablestack/management' },
+      // ],
+      // },
+      // {
+      //   type: 'group',
+      //   label: '관리플랫폼',
+      //   href: '/products/ablestack',
+      //   subMenuItems: [
+      //     { label: 'Mold 클라우드 관리', href: '/products/ablestack/mold' },
+      //     { label: 'Genie 하이브리드 배포 자동화', href: '/products/ablestack/genie' },
+      //     { label: 'Wall 통합 모니터링', href: '/products/ablestack/wall' },
+      //   ],
+      // },
+      // {
+      //   type: 'group',
+      //   label: '통합스토리지',
+      //   href: '/products/ablestack',
+      //   subMenuItems: [
+      //     { label: 'Block 블록스토리지', href: '/products/ablestack/block' },
+      //     { label: 'Files 파일시스템', href: '/products/ablestack/files' },
+      //     { label: 'Object 오브젝트 스토리지', href: '/products/ablestack/object' },
+      //     { label: 'Slio 백업 자동화', href: '/products/ablestack/slio' },
+      //   ],
+      // },
+      // {
+      //   type: 'group',
+      //   label: '서비스 및 플랫폼',
+      //   href: '/products/ablestack',
+      //   subMenuItems: [
+      //     { label: 'Works 가상데스크톱', href: '/products/ablestack/works' },
+      //     { label: 'Station 플랫폼서비스', href: '/products/ablestack/station' },
+      //   ],
+      // },
+      // {
+      //   label: 'ABLESTACK 어플라이언스',
+      //   href: '/products/ablestack',
+      // },
+      // {
+      //   label: 'ABLESTACK 조달제품',
+      //   href: '/products/ablestack',
+      // },
+    ],
+  },
+  {
+    label: '솔루션',
+    subMenuItems: [
       {
-        label: 'ABLESTACK 조달제품',
-        href: '/products/ablestack',
+        label: '프라이빗 클라우드',
+        href: '/solutions/private-cloud',
+      },
+      {
+        label: '하이브리드 클라우드',
+        href: '/solutions/hybrid-cloud',
+      },
+      {
+        label: '웹 애플리케이션',
+        href: '/solutions/webapp',
+      },
+      {
+        label: '빅데이터',
+        href: '/solutions/bigdata',
+      },
+      {
+        label: '영상감시',
+        href: '/solutions/monitoring',
+      },
+      {
+        label: 'IoT',
+        href: '/solutions/iot',
+      },
+      {
+        label: '엣지 컴퓨팅',
+        href: '/solutions/edge-computing',
+      },
+      {
+        label: '인공지능',
+        href: '/solutions/ai',
+      },
+      {
+        label: '최종 사용자 컴퓨팅',
+        href: '/solutions/euc',
       },
     ],
   },
-  // {
-  //   label: '솔루션',
-  //   subMenuItems: [
-  //     {
-  //       label: '프라이빗 클라우드',
-  //       href: '/solutions/private-cloud',
-  //     },
-  //     {
-  //       label: '하이브리드 클라우드',
-  //       href: '/solutions/hybrid-cloud',
-  //     },
-  //     {
-  //       label: '웹 애플리케이션',
-  //       href: '/solutions/webapp',
-  //     },
-  //     {
-  //       label: '빅데이터',
-  //       href: '/solutions/bigdata',
-  //     },
-  //     {
-  //       label: '영상감시',
-  //       href: '/solutions/monitoring',
-  //     },
-  //     {
-  //       label: 'IoT',
-  //       href: '/solutions/iot',
-  //     },
-  //     {
-  //       label: '엣지 컴퓨팅',
-  //       href: '/solutions/edge-computing',
-  //     },
-  //     {
-  //       label: '인공지능',
-  //       href: '/solutions/ai',
-  //     },
-  //     {
-  //       label: '최종 사용자 컴퓨팅',
-  //       href: '/solutions/euc',
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: '성공사례',
-  //   href: '/interview',
-  // },
+  {
+    label: '성공사례',
+    href: '/interview',
+  },
   {
     label: '파트너',
     href: '/partners',
   },
-  // {
-  //   label: '제품문의',
-  //   href: '/company/contact',
-  // },
-  // {
-  //   label: 'Blog',
-  //   href: '/blog',
-  // },
+  {
+    label: '제품문의',
+    href: '/company/contact',
+  },
+  {
+    label: '자료',
+    subMenuItems: [
+      {
+        label: 'Blog',
+        href: '/blog',
+      },
+      {
+        label: 'Docs',
+        href: '/blog',
+      },
+    ],
+  },
 ];
 
 export const footerMenus = [
@@ -199,32 +237,32 @@ export const footerMenus = [
       },
     ],
   },
-  // {
-  //   label: '솔루션',
-  //   subMenuItems: [
-  //     {
-  //       label: '-',
-  //       href: '/solutions',
-  //     },
-  //     {
-  //       label: '-',
-  //       href: '/solutions',
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: '성공사례',
-  //   subMenuItems: [
-  //     {
-  //       label: '-',
-  //       href: '/interview',
-  //     },
-  //     {
-  //       label: '-',
-  //       href: '/interview',
-  //     },
-  //   ],
-  // },
+  {
+    label: '솔루션',
+    subMenuItems: [
+      {
+        label: '-',
+        href: '/solutions',
+      },
+      {
+        label: '-',
+        href: '/solutions',
+      },
+    ],
+  },
+  {
+    label: '성공사례',
+    subMenuItems: [
+      {
+        label: '-',
+        href: '/interview',
+      },
+      {
+        label: '-',
+        href: '/interview',
+      },
+    ],
+  },
   {
     label: '파트너',
     subMenuItems: [
@@ -251,4 +289,48 @@ export const footerMenus = [
       },
     ],
   },
+];
+interface FooterItem {
+  label: string;
+  description: string;
+}
+
+export const footerInformation: FooterItem[][] = [
+  [
+    {
+      label: '회사명  ',
+      description: '(주)에이블클라우드',
+    },
+    {
+      label: '주소 본사 ',
+      description: '서울특별시 영등포구 영신로 220 KnK디지털타워 1808호',
+    },
+    {
+      label: '연구소 ',
+      description: '대전광역시 대덕구 대화로106번길 66 811호  ',
+    },
+  ],
+  [
+    {
+      label: '사업자등록번호 ',
+      description: '886-86-02158',
+    },
+    {
+      label: '대표 ',
+      description: '박동혁',
+    },
+    {
+      label: '전화 ',
+      description: '02-456-7667',
+    },
+    {
+      label: '팩스 ',
+      description: '02-456-7262',
+    },
+
+    {
+      label: 'Copyright (주)에이블클라우드. All Rights Reserved.',
+      description: '',
+    },
+  ],
 ];
