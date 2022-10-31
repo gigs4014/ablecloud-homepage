ad4986c36c3de91700cf06e44eca0879d659e0bf
Jaehun Kim <wogns3623@gmail.com>
2022-10-31 18:20:10 +0900

    FIX: pm2 ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,7 @@ module.exports = {
       args: 'start -p 80',
       interpreter: 'node',
       exec_mode: 'fork',
-      env,
-      wait_ready: true,
       autorestart: true,
-      listen_timeout: 50000,
-      kill_timeout: 5000,
     },
   ],
 };
