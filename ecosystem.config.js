3970f871b473757147dbd2dcc912942f3bad8c68
Jaehun Kim <wogns3623@gmail.com>
2022-10-31 19:25:55 +0900

    FEAT: fetch script
    - ecosystem.config.js 에 yarn 설치 경로를 찾아 자동으로 설정해주는 기능 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,14 @@
+const cp = require('child_process');
+
+let yarnPath = cp.execSync('whereis yarn', { encoding: 'utf8' });
+
+yarnPath = yarnPath.substring(yarnPath.indexOf(':') + 1);
+
 module.exports = {
   apps: [
     {
       name: `ablecloud-homepage`,
-      script: '/usr/local/bin/yarn',
+      script: yarnPath,
       args: 'start -p 80',
       interpreter: 'node',
       exec_mode: 'fork',
