dfde98821fd2aee54c2ed2aa151ef2eb1ef3ce5c
Jaehun Kim <wogns3623@gmail.com>
2022-10-31 20:01:47 +0900

    FIX: ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,14 @@
+const cp = require('child_process');
+
+let yarnPath = cp.execSync('whereis yarn', { encoding: 'utf8' });
+
+yarnPath = yarnPath.substring(yarnPath.indexOf(':') + 1).replace('\n', '');
+
 module.exports = {
   apps: [
     {
       name: `ablecloud-homepage`,
-      script: 'yarn',
-      args: 'start -p 80',
-      interpreter: 'node',
+      script: yarnPath + ' start -p 80',
       exec_mode: 'fork',
       autorestart: true,
     },
