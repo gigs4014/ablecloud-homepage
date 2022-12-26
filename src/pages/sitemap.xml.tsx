180584da3059f63ed9accb61a5f272432582c5b1
Kim jonhun <105820839+developerkimjh@users.noreply.github.com>
2022-12-26 14:43:14 +0900

    FIX: sitemap 수정

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -6,19 +6,27 @@ import { ISitemapField, getServerSideSitemap } from 'next-sitemap';
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const lastmod = new Date().toISOString();
-  const baseUrl = 'https://www.ablecloud.io';
+  const baseUrl = 'https://www.ablecloud.io/';
   const paths = await globby(['src/pages/**/index.tsx', '!src/pages/stories']);
-  const pages = paths.map(v => v.replace('src/pages', '').replace('/index.tsx', ''));
+  // const pages = paths.map(v => v.replace('src/pages', '').replace('/index.tsx', ''));
 
-  pages.sort();
+  const mainPages = paths.filter(v => v.split('/').length <= 4);
+  const subPages = paths.filter(v => v.split('/').length > 4);
+
+  const totalPages = [...mainPages, ...subPages.sort()];
+
+  const pages = totalPages.map(v =>
+    v.replace('src/pages/', '').replace('/index.tsx', '').replace('index.tsx', ''),
+  );
+
+  console.log(pages);
 
   const defaultFields: ISitemapField[] = pages.map(page => {
-    let loc;
-    if (!page) loc = `${baseUrl}/`;
-    else loc = `${baseUrl}${page}`;
+    const loc = `${baseUrl}${page}`;
     const sitemap = {
       loc,
       lastmod,
+      priority: page.includes('/') ? 0.9 : 1.0,
     };
     return sitemap;
   });
