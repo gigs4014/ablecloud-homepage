c1c4ef5dc1dd99fb502adc5312bee81c4658bf6d
Kim jonghun <developerkimjh@gmail.com>
2023-06-27 17:01:27 +0900

    FIX: home header 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react';
 
-import { ReactNode } from '@mdx-js/react/lib';
 import { NextSeo } from 'next-seo';
 
 import { Button, CustomLink } from '@/components/common';
-import { CommonDemoContent, HomeContent, HomeHeaderProps, HomeTemplate } from '@/templates';
+import {
+  CommonDemoContent,
+  HomeContent,
+  HomeHeaderProps,
+  HomeTemplate,
+  SliderData,
+} from '@/templates';
 
 import Logo_Article_2_1 from '@/public/images/new/home/article_2_1.svg';
 import Logo_Article_2_2 from '@/public/images/new/home/article_2_2.svg';
@@ -18,162 +23,41 @@ import Logo_Article_4_2 from '@/public/images/new/home/article_4_2.svg';
 import Logo_Article_4_3 from '@/public/images/new/home/article_4_3.svg';
 import Logo_Article_4_4 from '@/public/images/new/home/article_4_4.svg';
 import Logo_Article_4_5 from '@/public/images/new/home/article_4_5.svg';
+import Header_Content_1 from '@/public/images/new/home/header-image-1.png';
+import Header_Content_2 from '@/public/images/new/home/header-image-2.png';
+import Header_Content_3 from '@/public/images/new/home/header-image-3.png';
 
-import Video from '../../public/videos/data.mp4';
-
-interface LogoData {
-  title: string;
-  src: ReactNode;
-  href?: string;
-}
+const sliderData: SliderData[] = [
+  {
+    title: '단일 플랫폼으로 기존의 복잡한 인프라에 대한 고민 해결',
+    description:
+      'ABLESTACK은 서버, 스토리지, 네트워크 인프라를 제공하는 단일 HCI 플랫폼으로 기존 인프라 대비 IT운영 비용이 최대 80% 절감됩니다.',
+    href: '/about/infra',
+    bgImage: 'bg-home-header-bg-1',
+    contentImage: Header_Content_1,
+  },
+  {
+    title: '단일 플랫폼으로 기존의 복잡한 인프라에 대한 고민 해결',
+    description:
+      'ABLESTACK은 서버, 스토리지, 네트워크 인프라를 제공하는 단일 HCI 플랫폼으로 기존 인프라 대비 IT운영 비용이 최대 80% 절감됩니다.',
+    href: '/about/infra',
+    bgImage: 'bg-home-header-bg-2',
+    contentImage: Header_Content_2,
+  },
+  {
+    title: '단일 플랫폼으로 기존의 복잡한 인프라에 대한 고민 해결',
+    description:
+      'ABLESTACK은 서버, 스토리지, 네트워크 인프라를 제공하는 단일 HCI 플랫폼으로 기존 인프라 대비 IT운영 비용이 최대 80% 절감됩니다.',
+    href: '/about/infra',
+    bgImage: 'bg-home-header-bg-3',
+    contentImage: Header_Content_3,
+  },
+];
 
 const headerContents = (fontSize: string): HomeHeaderProps[] => [
   {
-    title: 'Cloud Data Center, Big Data',
-    description: (
-      <div className={`m-0 p-0 ${fontSize} font-[400] leading-[26.06px]`}>
-        클라우드 데이터센터 구축을 위한 통합 HCI 솔루션 ABLESTACK
-      </div>
-    ),
-    bgVideo: Video,
-    // slideImg: slideImg,
-    href: 'https://forms.gle/FLsHTEcEZ5X66hGe8',
-    href2: 'https://cic2023.framer.website/',
+    sliderData: sliderData,
   },
-  // {
-  //   title: '',
-  //   description: ``,
-  //   bgImage: 'bg-home-header-bg',
-  // },
-];
-
-const imageSliderSettings = {
-  dots: false,
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  arrows: false,
-  autoplay: true,
-  speed: 2000,
-  autoplaySpeed: 1000,
-  width: 850,
-  cssEase: 'linear',
-  responsive: [
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-  ],
-};
-
-const logos: Array<LogoData> = [
-  {
-    title: '지엔텔',
-    src: <Logo_Article_4_1 />,
-    href: 'http://www.lgntel.com/',
-  },
-  {
-    title: '지오멕스소프트',
-    src: <Logo_Article_4_2 />,
-    href: 'http://www.lgntel.com/',
-  },
-  {
-    title: '한국세무사회',
-    src: <Logo_Article_4_3 />,
-    href: 'http://www.lgntel.com/',
-  },
-  {
-    title: '근로복지공단',
-    src: <Logo_Article_4_4 />,
-    href: 'https://www.comwel.or.kr/',
-  },
-  {
-    title: '대검찰청',
-    src: <Logo_Article_4_5 />,
-    href: 'https://www.spo.go.kr/',
-  },
-  // {
-  //   title: '부안군',
-  //   src: Logo_buan,
-  //   href: 'https://www.buan.go.kr/',
-  // },
-  // {
-  //   title: '춘천시',
-  //   src: Logo_chuncheon,
-  //   href: 'https://www.chuncheon.go.kr/',
-  // },
-
-  // {
-  //   title: '다이소',
-  //   src: Logo_daiso,
-  //   href: 'https://www.daiso.co.kr/',
-  // },
-  // {
-  //   title: '광주문화재단',
-  //   src: Logo_gjcf,
-  //   href: 'http://www.gjcf.or.kr/',
-  // },
-
-  // {
-  //   title: '고령군',
-  //   src: Logo_goryeong,
-  //   href: 'https://www.goryeong.go.kr/',
-  // },
-  // {
-  //   title: '경상남도',
-  //   src: Logo_gyeongnam,
-  //   href: 'https://www.gyeongnam.go.kr/',
-  // },
-  // {
-  //   title: '인천광역시 교육청',
-  //   src: Logo_incheon_edu,
-  //   href: 'https://www.ice.go.kr/',
-  // },
-  // {
-  //   title: '인터넷 토마토',
-  //   src: Logo_internet_tomato,
-  //   href: 'https://www.emato.net/',
-  // },
-  // {
-  //   title: '전주시',
-  //   src: Logo_jeonju,
-  //   href: 'https://www.jeonju.go.kr/',
-  // },
-  // {
-  //   title: '전라남도 교육청',
-  //   src: Logo_jeonnam_edu,
-  //   href: 'https://www.jne.go.kr/',
-  // },
-  // {
-  //   title: '한국 스마트그리드 사업단',
-  //   src: Logo_ksgl,
-  //   href: 'https://www.smartgrid.or.kr',
-  // },
-  // {
-  //   title: 'kt ucloud biz',
-  //   src: Logo_kt_ucloud,
-  //   href: 'https://cloud.kt.com/',
-  // },
-
-  // {
-  //   title: '여주대학교',
-  //   src: Logo_yit,
-  //   href: 'https://www.yit.ac.kr/',
-  // },
 ];
 
 export default function HomePage() {
--- src/templates/home/HomeHeader.tsx
+++ src/templates/home/HomeHeader.tsx
@@ -1,8 +1,22 @@
+// import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/Ri';
 import { ReactNode } from 'react';
 
-// import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/Ri';
+import Image from 'next/image';
+
+import Slider from 'react-slick';
+
 import { BaseComponentProps } from '@/types';
 import { cls } from '@/utils';
+
+import { Button, CustomLink } from '@/components/common';
+
+export interface SliderData {
+  bgImage: string;
+  contentImage: StaticImageData;
+  href: string;
+  title: string;
+  description: string;
+}
 
 export interface HomeHeaderProps extends BaseComponentProps {
   title?: string;
@@ -10,22 +24,13 @@ export interface HomeHeaderProps extends BaseComponentProps {
   headerExtra?: ReactNode;
   bgImage?: string;
   textColor?: string;
-  bgVideo?: any;
-  slideImg?: any;
+  sliderData: SliderData[];
   href?: string;
-  href2?: string;
 }
 
 export default function HomeHeader({
-  title,
-  description,
-  headerExtra,
-  bgImage,
   className,
-  bgVideo,
-  slideImg,
-  href,
-  href2,
+  sliderData,
   textColor = 'text-white',
 }: HomeHeaderProps) {
   const options = {
@@ -40,59 +45,33 @@ export default function HomeHeader({
   };
 
   return (
-    <header
-      className={cls`not-prose relative flex h-full min-h-[670px] w-full justify-center overflow-hidden bg-cover ${bgImage} ${className}`}>
-      {/* <Slider {...options}> */}
-      {/* <div className='slide relative h-full h-[670px] w-full'>
-        <div className='z-10 flex h-full items-center justify-center pt-14'>
-          <section className={`${textColor} grid w-[340px] grid-cols-6 text-center md:w-[547px]`}>
-            <div className='col-span-4 mr-2 bg-[#350069] px-2 pt-2'>
-              <Image src={slideImg} />
+    <Slider {...options} className='not-prose'>
+      {sliderData.map(({ title, description, bgImage, contentImage, href }, index) => {
+        return (
+          <div key={index} className={cls`relative !flex h-[670px] w-full bg-cover ${bgImage}`}>
+            <div className='m-auto flex w-full max-w-[1400px] items-center justify-between'>
+              <div className='mx-[40px] text-white'>
+                <div className='mb-[32px] max-w-[455px] text-[32px] font-extrabold leading-[43.57px]'>
+                  {title}
+                </div>
+                <div className='mb-[32px] max-w-[594px] text-[16px] leading-[24.52px]'>
+                  {description}
+                </div>
+                <CustomLink href={href}>
+                  <Button
+                    className={'mt-5 w-full max-w-[240px] !border-[white] !text-[white]'}
+                    bordered>
+                    자세히보기
+                  </Button>
+                </CustomLink>
+              </div>
+              <div className='hidden lg:flex'>
+                <Image src={contentImage} />
+              </div>
             </div>
-            <div className='slide-font col-span-2 bg-[#350069] px-3 py-1 text-left text-[14px] font-[700] leading-[18px] md:text-[22px] md:leading-[26px]'>
-              CLOUD
-              <br />
-              INFINITY
-              <br />
-              CONFERENCE
-            </div>
-            <p className='slide-font col-span-6 mt-4 flex-none text-[14px] font-[400] leading-[15px] md:text-[22px] md:leading-[29px]'>
-              함께할 때 무한히 확장되는 가능성, ABLESTACK 에코 플랫폼
-            </p>
-            <p className='slide-font col-span-6 my-6 text-[16px] font-[700] leading-[25px] md:my-10 md:text-[24px] md:leading-[36px]'>
-              2023.03.22 (수) 14:00 <br />더 세인트 구로 그랜드볼룸(6F)
-            </p>
-            <a className='col-span-3 pr-3' href={href}>
-              <button className='slide-cont slide-font radius h-[35px] w-full bg-white text-[18px] leading-[21px] text-[rgb(123,1,244)] md:h-[48px]'>
-                사전 신청 하기
-              </button>
-            </a>
-            <a className='col-span-3 pl-3' href={href2}>
-              <button className='slide-cont slide-font radius h-[35px] w-full bg-[rgb(123,1,244)] text-[18px] md:h-[48px]'>
-                자세히 보기
-              </button>
-            </a>
-          </section>
-        </div>
-      </div> */}
-
-      <div className='relative h-[670px] w-full'>
-        <video muted autoPlay loop playsInline className='absolute h-full w-full object-cover'>
-          <source src={bgVideo} type='video/mp4' />
-        </video>
-        <div className='z-11 relative flex h-full w-full items-center justify-center space-x-8 px-8'>
-          <section className={`${textColor} text-center`}>
-            <p className={'m-0 pt-24 pb-8 text-[42px] font-[800] leading-[52.42px]'}>{title}</p>
-            {typeof description === 'string' ? (
-              <p className={'m-0 text-[18px] font-[400] leading-[26.06px]'}>{description}</p>
-            ) : (
-              description
-            )}
-            {headerExtra && <div className={'mt-[40px] bg-transparent'}>{headerExtra}</div>}
-          </section>
-        </div>
-      </div>
-      {/* </Slider> */}
-    </header>
+          </div>
+        );
+      })}
+    </Slider>
   );
 }
