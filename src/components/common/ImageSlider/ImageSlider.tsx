f398d670b3e0d7ddf7beeab022704ed35a19ce0e
Kim jonghun <developerkimjh@gmail.com>
2024-03-05 11:25:06 +0900

    FIX: fix home header content

---
+++ src/components/common/ImageSlider/ImageSlider.tsx
@@ -0,0 +1,18 @@
+('use client');
+
+const ImageSlider = () => {
+  return (
+    <div className='scroll imgBox hidden xsm:flex'>
+      <div>
+        <img src='/images/new/home/header-image-4.png' />
+        <img src='/images/new/home/header-image-4.png' />
+      </div>
+      <div>
+        <img src='/images/new/home/header-image-4.png' />
+        <img src='/images/new/home/header-image-4.png' />
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
--- src/templates/home/Home.tsx
+++ src/templates/home/Home.tsx
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from 'react';
 
-import { v4 as uuid } from 'uuid';
-
 import { BaseComponentProps } from '@/types';
 
+import { Button } from '@/components/common';
+import ImageSlider from '@/components/common/ImageSlider/ImageSlider';
 import { Container } from '@/components/layout';
 
-import HomeHeader, { HomeHeaderProps } from './HomeHeader';
+import { HomeHeaderProps } from './HomeHeader';
 
 export interface HomeTemplateProps extends BaseComponentProps {
   enableProseSpacing?: boolean;
@@ -34,9 +34,32 @@ function HomeTemplate({
     <>
       <Container.Article enableProseSpacing={enableProseSpacing} className={className}>
         {/* <CustomSlider settingsOverrides={headerSliderSettings}> */}
-        {headerContents.map(headerContent => (
+
+        <div className='not-prose relative h-[560px] bg-home-header-bg-4 bg-cover'>
+          <div className='flex flex-col items-center justify-center gap-[50px] pt-[120px]'>
+            <div className='flex flex-col items-center gap-4 px-4'>
+              <div className='text-center text-[30px] font-extrabold text-white lg:text-[42px]'>
+                CLOUD INFINITY CONFERENCE 2024
+              </div>
+              <div className='text-center text-[22px] font-bold text-[#18EE94] lg:text-[30px]'>
+                클라우드 인피니티 컨퍼런스 2024에 귀하를 초대합니다.
+              </div>
+            </div>
+            <Button
+              className='bg-[#18EE94] px-[72px] text-[16px] !font-bold text-black'
+              onClick={() => {
+                window.open('https://cic2024.framer.website/', '_blank');
+              }}>
+              사전 신청 하기
+            </Button>
+
+            <ImageSlider />
+          </div>
+        </div>
+
+        {/* {headerContents.map(headerContent => (
           <HomeHeader key={uuid()} {...headerContent} />
-        ))}
+        ))} */}
         {/* </CustomSlider> */}
 
         {children}
